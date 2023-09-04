26179421a2c8c24d8fa0794823c031ba10286aec
KiattisakDeeprasert <142884142+KiattisakDeeprasert@users.noreply.github.com>
2023-09-04 09:30:30 +0000

    gg

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -2,6 +2,7 @@ package th.mfu;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,57 +16,42 @@ public class BMICalculatorServlet extends HttpServlet{
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO: get parameter from request: "weight" and "height"
-        String weightStr = request.getParameter("weight");
-        String heightStr = request.getParameter("height");
+        String weight = request.getParameter("weight");
+        String height = request.getParameter("height");
 
-        if (weightStr != null && !weightStr.isEmpty() && heightStr.isEmpty()) {
+        //TODO: calculate bmi
+        if (weight != null && height != null && !weight.isEmpty() && !height.isEmpty()) {
             try {
-                 double weight = Double.parseDouble(weightStr);
-                 double height = Double.parseDouble(heightStr);
-         
-                 //TODO: calculate bmi
-                 double bmi = calculateBMI(weight, height);
-
-                 //TODO: determine the built from BMI
-                 String build = determineBuild(bmi);
-                  //TODO: add bmi and built to the request's attribute
-                 request.setAttribute("bmi", Math.round(bmi));
-                 request.setAttribute("build", build);
-                 //TODO: forward to jsp
-                 
-                 request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
-
-            } catch (Exception e) {
-                // TODO: handle exception
-                response.getWriter().println("Invalid weight or hei values");
+                double weightValue = Double.parseDouble(weight);
+                double heightValue = Double.parseDouble(height);
+                //calculate bmi
+                double bmi = weightValue / (heightValue * heightValue);
+                //TODO: determine the built from BMI
+                String Build = determineBuild(bmi);
+                //TODO: add bmi and built to the request's attribute
+                request.setAttribute("bmi",Math.round(bmi));
+                request.setAttribute("build", Build);
+                request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
+            }catch (Exception e){
+                response.getWriter().println("Invalid input");
             }
-            
-        } else {
-            response.getWriter().println("please provide weight and height parameters");
+        }else{
+            response.getWriter().println("Please provide weight and height parameters.");
         }
-       
     }
-     private double calculateBMI(double weight, double height){
-         return weight / ((height)*(height));   
-        }
-      
-        private String determineBuild(double bmi){
-            if(bmi < 18.5) {
-            return "underweight";
-        } else if (bmi < 25) {
-            return "normal weight";
-        } else if (bmi <30) {
-            return "overweight";
-        } else if(bmi < 35){
-            return "obese";
 
-        } 
-         else {
+    private String determineBuild(double bmi) {
+        if( bmi < 18.5){
+            return "underweight";
+        }else if(bmi < 25){
+            return "normal";
+        }else if( bmi < 30){
+            return "overweight";
+        }else if(bmi < 35){
+            return "obese";
+        }else{
             return "extremely obese";
         }
-      
-        
-        
     }
     
 }
